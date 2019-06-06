3940444b7d98ef54fcee3686dbcfa87aeee92e4a
Joseph Cuffney <josephcuffney@gmail.com>
2019-06-06 15:28:16 -0700

    react standard

--- react.js
+++ react.js
@@ -1,21 +1,21 @@
 'use strict';
 
 module.exports = {
-  extends: './node.js',
-  plugins: ['react'],
-  env: {
-    browser: true,
-    'shared-node-browser': true,
-    node: false,
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  globals: {
-    module: false,
-  },
-  rules: {},
+  extends: ['eslint-config-standard', 'eslint-config-standard-react'],
+  // plugins: ['react'],
+  // env: {
+  //   browser: true,
+  //   'shared-node-browser': true,
+  //   node: false,
+  // },
+  // parserOptions: {
+  //   sourceType: 'module',
+  //   ecmaFeatures: {
+  //     jsx: true,
+  //   },
+  // },
+  // globals: {
+  //   module: false,
+  // },
+  // rules: {},
 };
