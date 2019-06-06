effb91c4d564bb487f8e409bbebc724a16229ece
Joseph Cuffney <josephcuffney@gmail.com>
2019-06-06 14:30:18 -0700

    clean configs

--- node-test.js
+++ node-test.js
@@ -7,10 +7,5 @@ module.exports = {
     mocha: true,
   },
 
-  rules: {
-    'max-statements': [1, 50],
-    'no-console': [0],
-    'no-underscore-dangle': [0],
-    'prefer-arrow-callback': [0],
-  },
+  rules: {},
 };
